75f60ae04332f64548a9e8e29301036d0a2cdcd5
Mike Zwartz <mtzwartz@gmail.com>
2022-11-28 22:24:14 -0600

    created command file and updated sales data

---
+++ commands.sql
@@ -0,0 +1,14 @@
+--SQL COMMANDS--
+--two insert statements
+
+--two update statements
+
+--one delete statement
+
+--one select statment for subset of rows and columns from one table 
+
+--two select statements from joining of two tables
+
+--three summary functions 
+
+--one multi-table query
