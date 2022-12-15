e44732964bffea00ce1961e53a5d1bc8e1828399
Mike Zwartz <mtzwartz@gmail.com>
2022-12-14 21:30:26 -0600

    updated

--- python_script.py
+++ python_script.py
@@ -1,9 +1,33 @@
 import mysql.connector
+from mysql.connector import errorcode
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user='root',
-    password="password"
-    )
+try: 
+    cm_connection = mysql.connector.connect(
+        user='localhost',
+        password = 'password',
+        host = '127.0.0.1',
+        database="timmzatlantic"
+        )
+    
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR: 
+        print("Invalid credentials")
+    elif err.erno == errorcode.ER_BAD_DB_ERROR:
+        print("Database not found")
+    else:
+        print("Cannot connect to database: ", err)
+    exit()
 
-print(mydb)
+#Part I: Print entire table
+sales_data_cursor = cm_connection.cursor()
+sales_data_query = ("SELECT * FROM sales_data")
+sales_data_cursor.execute(sales_data_query)
+
+for row in sales_data_cursor.fetchall():
+    print(row)
+
+sales_data_cursor.close()
+
+
+#Close connection 
+cm_connection.close() 
